acee030ebf3e28d01d60e82ce1afe35b0ea91ffd
strngluv <strngluv@example.com>
2018-06-03 17:48:53 +0800

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -17,12 +17,39 @@ socket.on('newMessage', function (message) {
 	$('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+	var li = $('<li></li>');
+	var a = $('<a target="_blank">My current location</>');
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+
+	$('#messages').append(li);
+});
+
 $('#message-form').on('submit', function (e) {
 	e.preventDefault();
 
 	socket.emit('createMessage', {
-		name: 'User',
+		from: 'User',
 		text: $('[name=message]').val()
 	}, function () {
 	})
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function (e) {
+	if (!navigator.geolocation) {
+		return alert('Geolocation not supported by your browser');
+	}
+
+	navigator.geolocation.getCurrentPosition(function (position) {
+		console.log(position);
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude,
+		});
+	}, function (err) {
+		alert('Unable to fetch location');
+	});
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,28 @@
 const expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate the correct message object', () => {
 		var from = 'Shannon';
 		var text = 'I feel fantasic!';
 
-		var result  = generateMessage(from, text);
+		var message   = generateMessage(from, text);
 
-		expect(result).toMatchObject({from, text});
-		expect(typeof result.createdAt).toBe('number');
+		expect(message ).toMatchObject({from, text});
+		expect(typeof message .createdAt).toBe('number');
 	})
 });
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var from = 'Eric';
+		var latitude = 1;
+		var longitude = 2;
+		var url = 'https://www.google.com/maps?q=1,2';
+
+		var message = generateLocationMessage(from, latitude, longitude);
+
+		expect(message ).toMatchObject({from, url});
+		expect(typeof message .createdAt).toBe('number');
+	})
+})
